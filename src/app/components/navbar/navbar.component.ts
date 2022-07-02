69c24c422d84583235031c6d5917469f14a62fb3
James Yeap <james.yeap@accenture.com>
2022-07-02 12:14:55 +0800

    Fix styling for Nav Bar component

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,19 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {AuthService} from "../../modules/auth/services/auth.service";
+import {createPopper} from "@popperjs/core";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  public isHidden:boolean = true;
+export class NavbarComponent implements AfterViewInit {
+  public showDropdownPopover:boolean = false;
+
+  @ViewChild("btnDropdownRef", { static: false })
+  btnDropdownRef!: ElementRef;
+  @ViewChild("popoverDropdownRef", { static: false })
+  popoverDropdownRef!: ElementRef;
+
+  popperInstance:any;
 
   constructor(
     private authService:AuthService
   ) { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.popperInstance = createPopper(
+      this.btnDropdownRef.nativeElement,
+      this.popoverDropdownRef.nativeElement,
+      {
+        placement:"auto"
+      }
+    )
+  }
+
+  toggleDropdown(event:any) {
+    event.preventDefault();
+
+    if (this.showDropdownPopover) {
+      this.showDropdownPopover = false;
+    } else {
+      this.showDropdownPopover = true;
+    }
   }
 
   isLoggedIn(): boolean {
