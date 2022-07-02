5221f4981a91839667047a25b8651b8741b39c91
James Yeap <james.yeap@accenture.com>
2022-07-02 11:22:07 +0800

    Reorganise files

--- src/app/components/my-account/my-account.component.ts
+++ src/app/components/my-account/my-account.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  templateUrl: './my-account.component.html',
+  styleUrls: ['./my-account.component.css']
 })
-export class HomeComponent implements OnInit {
+export class MyAccountComponent implements OnInit {
 
   constructor() { }
 
