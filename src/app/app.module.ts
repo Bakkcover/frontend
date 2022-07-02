444447fa6097ccfba7b32e74b8cd2200986444e2
James Yeap <james.yeap@accenture.com>
2022-07-02 11:23:09 +0800

    Import reactive form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {AuthModule} from "./modules/auth/auth.module";
-import { HomeComponent } from './components/home/home.component';
+import { MyAccountComponent } from './components/my-account/my-account.component';
 import { LoggedInGuard } from './loggedin.guard';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HowItWorksComponent } from './components/how-it-works/how-it-works.component';
+import { LibraryComponent } from './components/library/library.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NavbarComponent
+    MyAccountComponent,
+    NavbarComponent,
+    HowItWorksComponent,
+    LibraryComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     AuthModule
   ],
   providers: [LoggedInGuard],
