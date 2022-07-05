d3b406ee657a51209da52430569d859743342d58
James Yeap <james.yeap@accenture.com>
2022-07-05 17:21:05 +0800

    Generate Add Book module and component

---
+++ src/app/add-book/add-book.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddBookComponent } from './add-book.component';
+
+
+
+@NgModule({
+  declarations: [
+    AddBookComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddBookModule { }
