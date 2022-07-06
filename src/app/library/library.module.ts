375ed2202cfcb0d0a16b2c674ff9d3e6271b4dfe
James Yeap <james.yeap@accenture.com>
2022-07-06 17:57:36 +0800

    Generate Book Details component

--- src/app/library/library.module.ts
+++ src/app/library/library.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookListComponent } from './components/book-list/book-list.component';
 import {BookListItemComponent} from "./components/book-list-item/book-list-item.component";
+import { BookDetailsComponent } from './components/book-details/book-details.component';
 
 @NgModule({
   declarations: [
     BookListComponent,
-    BookListItemComponent
+    BookListItemComponent,
+    BookDetailsComponent
   ],
   exports: [
     BookListComponent
